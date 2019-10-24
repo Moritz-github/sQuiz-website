6779b3070b425aedf670d326a5b97e12a78af184
Moritz Laichner <moritz@laichner.com>
2019-10-24 19:16:58 +0200

    imlementation front-end of new database system

--- app/models.py
+++ app/models.py
@@ -2,29 +2,29 @@ from app import app
 from app import db
 
 class User(db.Model):
-    userID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
 
     quizzes = db.relationship('Quiz', backref="author", lazy=True)
 
     def __repr__(self):
-        return f"User('{self.userID}', '{self.username}')"
+        return f"User('{self.id}', '{self.username}')"
 
 class Quiz(db.Model):
-    quizID = db.Column(db.Integer, primary_key=True)
-    quizname = db.Column(db.String(100), nullable=False)
-    authorID = db.Column(db.Integer, db.ForeignKey("user.userID"), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    authorID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
     questions = db.relationship("Question", backref="quiz", lazy=True)
 
     def __repr__(self):
-        return f"Quiz('{self.quizID}', '{self.quizname}')"
+        return f"Quiz('{self.id}', '{self.name}')"
 
 class Question(db.Model):
-    questionID = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     answer = db.Column(db.String(100), nullable=False)
-    quizID = db.Column(db.Integer, db.ForeignKey("quiz.quizID"), nullable=False)
+    quizID = db.Column(db.Integer, db.ForeignKey("quiz.id"), nullable=False)
 
     def __repr__(self):
-        return f"Question('{self.questionID}', '{self.content}', '{self.answer}')"
+        return f"Question('{self.id}', '{self.content}', '{self.answer}')"
