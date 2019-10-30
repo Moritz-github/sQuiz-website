5d9732854e0539ae55d50053715323471767e3f2
Moritz Laichner <moritz@laichner.com>
2019-10-30 10:16:34 +0100

    removed unnecessary old files

--- app/questions/question_pools.py
+++
@@ -1,9 +0,0 @@
-from app.questions.pool import question_pool
-from app.questions.chemistry import chemistry_question
-from app.questions.amec import amec_functions
-
-pool_chemistry = question_pool(chemistry_question, "Chemie")
-
-pool_amec = question_pool(amec_functions, "AMec")
-
-question_pools = {"Amec": pool_amec, "Chemie": pool_chemistry}
